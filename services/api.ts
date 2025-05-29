58e29748a25e903725bc81c4a0efe165db10ff8f
Kauann Henrique <kauannsantos013@gmail.com>
2025-05-29 17:24:35 -0300

    atualização do front

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
   import axios from 'axios';
 
   const api = axios.create({
-    baseURL: 'http://localhost:5263/api',
+    baseURL: 'http://192.168.1.9:5263/api',
     headers: {
       'Content-Type': 'application/json',
 
